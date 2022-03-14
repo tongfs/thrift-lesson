1a8a100bca5449b796e499ec3b6ffa3f7cc59832
tongfs <reg.tong@outlook.com>
2022-03-14 15:33:51 +0800

    Implement multithreaded service

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -3,12 +3,17 @@
 
 #include "match_server/Match.h"
 #include "save_client/Save.h"
+#include <thrift/concurrency/ThreadManager.h>
+#include <thrift/concurrency/ThreadFactory.h>
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
+#include <thrift/server/TThreadPoolServer.h>
+#include <thrift/server/TThreadedServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TTransportUtils.h>
+#include <thrift/TToString.h>
 
 #include <iostream>
 #include <thread>
@@ -161,6 +166,25 @@ class MatchHandler : virtual public MatchIf {
 };
 
 
+class MatchCloneFactory : virtual public MatchIfFactory {
+    public:
+        ~MatchCloneFactory() override = default;
+        MatchIf* getHandler(const ::apache::thrift::TConnectionInfo& connInfo) override
+        {
+            std::shared_ptr<TSocket> sock = std::dynamic_pointer_cast<TSocket>(connInfo.transport);
+            //cout << "Incoming connection\n";
+            //cout << "\tSocketInfo: "  << sock->getSocketInfo() << "\n";
+            //cout << "\tPeerHost: "    << sock->getPeerHost() << "\n";
+            //cout << "\tPeerAddress: " << sock->getPeerAddress() << "\n";
+            //cout << "\tPeerPort: "    << sock->getPeerPort() << "\n";
+            return new MatchHandler;
+        }
+        void releaseHandler(MatchIf* handler) override {
+            delete handler;
+        }
+};
+
+
 void consume_task()
 {
     while (true)
@@ -189,16 +213,13 @@ void consume_task()
 
 
 int main(int argc, char **argv) {
-    int port = 9090;
-    ::std::shared_ptr<MatchHandler> handler(new MatchHandler());
-    ::std::shared_ptr<TProcessor> processor(new MatchProcessor(handler));
-    ::std::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-    ::std::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-    ::std::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+    TThreadedServer server(
+            std::make_shared<MatchProcessorFactory>(std::make_shared<MatchCloneFactory>()),
+            std::make_shared<TServerSocket>(9090),
+            std::make_shared<TBufferedTransportFactory>(),
+            std::make_shared<TBinaryProtocolFactory>());
 
-    TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
-
-    cout << "match_server starts!" << endl;
+    cout << "match server starts!" << endl;
 
     thread match_thread(consume_task);
 
